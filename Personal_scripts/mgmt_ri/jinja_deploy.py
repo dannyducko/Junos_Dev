e645ecb34205d5dd43d3f283332971cff8e72c2d
dannyducko <danny@ducko.uk>
2022-04-27 01:50:26 +0100

    ri mgmt scripts

---
+++ Personal_scripts/mgmt_ri/jinja_deploy.py
@@ -0,0 +1,18 @@
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+import yaml
+import creds
+
+# Below are DNS entries for my lab.
+junos_hosts = ['lab.srx1', 'lab.srx2']
+
+for host in junos_hosts:
+    filename = 'Personal_scripts/mgmt_ri/srx.yml'
+    with open(filename, 'r') as fh:
+        data = yaml.safe_load(fh)
+        with Device(host, user=creds.username, passwd=creds.password) as dev:
+            with Config(dev, mode="exclusive") as conf:
+                conf.load(template_path="Personal_scripts/mgmt_ri/mgmt_ri.j2", template_vars=data, format="text")
+                conf.pdiff()
+                conf.commit()
+            print("Configuration loaded succesfully.")
