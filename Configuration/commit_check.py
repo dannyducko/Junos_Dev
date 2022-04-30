7952d84889028329fc2d214e0eeef88597aa1619
dannyducko <danny@ducko.uk>
2022-04-30 17:37:23 +0000

    t

--- Configuration/commit_check.py
+++ Configuration/commit_check.py
@@ -6,3 +6,4 @@ with Device(host='IP_HERE', user='USERNAME', passwd='PASSWORD') as srx1:
             cu.load(path="Configuration/confirm.conf", overwrite=True)
             cu.commit_check()
             print("Device configuration has been confirmed.")
+            #
